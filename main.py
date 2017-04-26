8e551288ddba1bbbd7e825831b6ae5e474682dba
Euan Ramsay <euancramsay@aol.com>
2017-04-26 16:11:17 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import webapp2
+import os
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'signup.html') 
+        self.response.out.write(template.render(path, {}))        
+
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
