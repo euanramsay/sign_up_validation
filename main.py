f99559b8fd0c908dc7ea09fd83ef16ecaeb3245f
Euan Ramsay <euancramsay@aol.com>
2017-04-28 10:48:13 +0100

    sorted validation

--- main.py
+++ main.py
@@ -1,11 +1,64 @@
 import webapp2
 import os
 from google.appengine.ext.webapp import template
+import re
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        path = os.path.join(os.path.dirname(__file__), 'signup.html') 
-        self.response.out.write(template.render(path, {}))        
+        path = os.path.join(os.path.dirname(__file__), 'templates/signup.html')
+        self.response.out.write(template.render(path, {"message": "Please enter your details"}))
 
+    def post(self):
+        path = os.path.join(os.path.dirname(__file__), 'templates/signup.html')
+        have_Error = False
+        username_input = self.request.get("username")
+        password_input = self.request.get("password")
+        verify_input = self.request.get("verify")
+        email_input = self.request.get("email")
+
+        username_error = ""
+        password_error = ""
+        verify_error = ""
+        email_error = ""
+
+        if not valid_username(username_input):
+            username_error = "This is not a valid username!"
+            have_Error = True
+
+        if not valid_password(password_input):
+            password_error = "This is not a valid password!"
+            have_Error = True
+        elif password_input!=verify_input:
+            verify_error = "Passwords did not match"
+
+        if not valid_email(email_input):
+            email_error = "This is not a valid email"
+            have_Error = True
+    
+        if have_Error:
+            self.response.out.write(template.render(path, {"message": "Please correct mistakes", 
+                "username_error": username_error,
+                "password_error": password_error,
+                "verify_error": verify_error,
+                "email_error": email_error
+                }))
+        else:
+            self.response.out.write(template.render(path, {"message": "Well done!"}))
+        
+
+       
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
